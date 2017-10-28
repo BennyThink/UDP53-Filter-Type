9bef2b9a76173a5a267d0fcd8a09247a46453203
Benny <benny.think@gmail.com>
2017-10-28 17:28:06 +0800

    Python 3 support and functionalize

--- UDP53.py
+++ UDP53.py
@@ -1,49 +1,71 @@
-# coding: utf8
+#!/usr/bin/python
+# coding:utf-8
+
+# UDP53-Filter-Type - UDP53.py
+# 2017/10/28 16:58
+# Test for Port 53, support both Python 2 and Python 3 under Linux & Windows.
+
+__author__ = 'Benny <benny@bennythink.com>'
+
 import socket
 
 HOST = '123.206.87.223'
 PORT = 53
 
 
-def send_data(data):
+def send_data():
+    err = None
+    print('Start Testing...')
     sock_clt = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock_clt.connect((HOST, PORT))
-    sock_clt.send(data)
-    sock_clt.close()
+    try:
+        sock_clt.connect((HOST, PORT))
+        sock_clt.send('Hi')
+    except socket.error as e:
+        err = e
+    except ConnectionRefusedError as e:
+        err = e
+    finally:
+        sock_clt.close()
+
+    return str(err)
 
 
-print
-print '============= Benny\'s UDP 53 Filter Type Test =============='
-print
-print 'In this program, you could test your hotspot\'s interception '
-print 'of UDP 53.'
-print
-print 'If your hostpot allows any kind of UDP Packet via Port 53,   '
-print 'you may wanna setup an OpenVPN server to bypass Web Auth;    '
-print 'if not, DNS Tunnel is actually a bad option.'
-print
-print '                                     By Benny with love 2017 '
-print '                                   https://www.bennythink.com'
-print '============================================================='
-print
-print '----------------------  Start Testing  ----------------------'
+def parser(err_msg):
+    if '10061' in err_msg or '111' in err_msg:
+        return True
+    elif '10060' in err_msg or '110' in err_msg:
+        return False
+    else:
+        return None
 
-try:
-    send_data('Hi')
-except socket.error as e:
-    print
-else:
-    print "That cannot happen!"
 
-if '10061' in str(e) or '111' in str(e):
-    print ':-) Congratulations, you may wanna try OpenVPN!'
-    print 'Visit https://www.bennythink.com/udp53.html for more info.\n'
-    raw_input('Press Enter to exit.')
-elif '10060' in str(e) or '110' in str(e):
-    print ':-) Sad face, but DNS Tunnel is an option.'
-    print 'Visit https://www.bennythink.com/udp53.html for more info.\n'
-    raw_input('Press Enter to exit.')
-else:
-    print ':-('
-    print 'You probably forget to connect to a Wi-Fi hotspot.\n'
-    raw_input('Press Enter to exit.')
+def deter(err_stat):
+    if err_stat:
+        print(':-) Congratulations, you may wanna try OpenVPN!')
+        print('Visit https://www.bennythink.com/udp53.html for more info.\n')
+    elif err_stat is False:
+        print(':-) Sad face, but DNS Tunnel is an option.')
+        print('Visit https://www.bennythink.com/udp53.html for more info.\n')
+    else:
+        print(':-(\nYou probably forget to connect to a Wi-Fi hotspot.\n')
+
+
+def welcome():
+    print('============= Benny\'s TCP&UDP 53 Filter Type Test ==========\n')
+    print('In this program, you could test your hotspot\'s interception ')
+    print('of UDP 53.\n')
+    print('If your hotspot allows any kind of  Packet via Port 53,   ')
+    print('you may wanna setup an OpenVPN or Shadowsocks server ')
+    print('to bypass Web Auth;    ')
+    print('if not, DNS Tunnel is actually a bad option.\n')
+    print('                                     By Benny with love 2017 ')
+    print('                                   https://www.bennythink.com')
+    print('=============================================================')
+
+
+if __name__ == '__main__':
+    welcome()
+    res1 = parser(send_data())
+    deter(res1)
+
+    input('Press Enter to exit.')
